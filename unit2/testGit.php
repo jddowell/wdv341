5a22f6fc81344433fcc9957d26a00cf0e5ef20cf
justin <jddowell@dmacc.edu>
2018-02-27 16:13:44 -0600

    git test

---
+++ unit2/testGit.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<?php $myName = "Justin Dowell" ?>
+	<?php echo "<h1>$myName</h1>" ?>
+</body>
+</html>
