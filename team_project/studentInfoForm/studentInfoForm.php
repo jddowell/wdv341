4dfb3c19d7645cecac75eebb9c20388af9052d6f
justin <jddowell@dmacc.edu>
2018-04-02 13:53:49 -0500

    team project

--- team_project/studentInfoForm/studentInfoForm.php
+++ team_project/studentInfoForm/studentInfoForm.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<?php print_r($_POST); ?>
+<!-- <?php print_r($_POST); ?> -->
 <head>
   <meta charset="utf-8">
   <title>DMACC Portfolio Day Bio Form</title>
@@ -14,13 +14,14 @@
   <!-- css3-mediaqueries.js for IE less than 9 -->
 
 <script src="css3-mediaqueries.js"></script>
-<script src="jquery-3.2.1.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <?php
 	//Variables
 	$student_emailToLogin = "";
 	$student_firstname = "";
 	$student_lastname = "";
 	$student_program = "";
+	$student_program2 = "";
 	$student_websiteAddress = "";
 	$student_websiteAddress2 = "";
 	$student_email = "";
@@ -40,8 +41,10 @@
 	$hometownError = "";
 	$careerGoalsError = "";
 	$threeWordsError = "";
+	$captchaError = "";
 	
 	$validForm = false;
+	$message = "";
 	
 	if(isset($_POST["submit"]))
 	{
@@ -49,6 +52,7 @@
 		$student_firstname = $_POST['firstName'];
 		$student_lastname = $_POST['lastName'];
 		$student_program = $_POST['program'];
+		$student_program2 = $_POST['program2'];
 		$student_websiteAddress = $_POST['websiteAddress'];
 		$student_websiteAddress2 = $_POST['websiteAddress2'];
 		$student_email = $_POST['email'];
@@ -56,30 +60,41 @@
 		$student_hometown = $_POST['hometown'];
 		$student_careerGoals = $_POST['careerGoals'];
 		$student_threeWords = $_POST['threeWords'];
+		if(isset($_POST['g-recaptcha-response'])){
+	      $captcha = $_POST['g-recaptcha-response'];
+	    }
 	
 	
-			function validateFirstName($inName)
+		function validateFirstName($inName)
+		{
+			global $validForm, $firstNameError;	
+			$firstNameError = "";
+			
+			if($inName == "")
 			{
-				global $validForm, $firstNameError;	
-				$firstNameError = "";
-				
-				if($inName == "")
-				{
-					$validForm = false;
-					$firstNameError = "Name cannot be spaces";
-				}
+				$validForm = false;
+				$firstNameError = "First name is required";
+			}else if(!preg_match('/^[A-Za-z0-9 ]*[A-Za-z0-9][A-Za-z0-9 ]*$/', $inName)){
+				$validForm = false;
+				$firstNameError = "First name cannot contain special characters";
 			}
-			function validateLastName($inName)
+		}
+
+		function validateLastName($inName)
+		{
+			global $validForm, $lastNameError;	
+			$lastNameError = "";
+			
+			if($inName == "")
 			{
-				global $validForm, $lastNameError;	
-				$lastNameError = "";
-				
-				if($inName == "")
-				{
-					$validForm = false;
-					$lastNameError = "Name cannot be spaces";
-				}
+				$validForm = false;
+				$lastNameError = "Last name is required";
+			}else if(!preg_match('/^[A-Za-z0-9 ]*[A-Za-z0-9][A-Za-z0-9 ]*$/', $inName)){
+				$validForm = false;
+				$lastNameError = "Last name cannot contain special characters";
 			}
+		}
+
 		function validateProgram($program)
 		{
 			global $validForm, $programError;
@@ -91,94 +106,128 @@
 				$programError = "Must select an option";
 			}
 		}
+
 		function validateWebsiteAddress($address)
 		{
 			global $validForm, $websiteAddressError;
 			
 			$websiteAddressError = "";
 			
-			if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$address))
-			{
-				$validForm = false;
-				$websiteAddressError = "Invalid URL";
+			if(!empty($address)){
+				if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$address))
+				{
+					$validForm = false;
+					$websiteAddressError = "Invalid URL";
+				}
 			}
 		}
+
 		function validateWebsiteAddress2($address2)
 		{
 			global $validForm, $websiteAddress2Error;
 			
 			$websiteAddressError2 = "";
 			
-			if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$address2))
-			{
-				$validForm = false;
-				$websiteAddress2Error = "Invalid URL";
+			if(!empty($address2)){
+				if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$address2))
+				{
+					$validForm = false;
+					$websiteAddress2Error = "Invalid URL";
+				}
 			}
 		}
+
 		function validatelinkIn($linkedin)
 		{
 			global $validForm, $linkinError;
 			//echo("here".$linkedin.strpos($linkedin , "linkedin.com"));
-			if($linkedin != "")
-			{
+			if(!empty($linkedin)){
 				if(!strstr($linkedin , "linkedin.com"))
 				{
 					$validForm = false;
-					$linkinError = "not correct linkedin URL";
-			}
+					$linkinError = "Invalid LinkedIn URL";
+				}
 			}
 		}
+
 		function validateEmail($inEmail)
 		{
-				global $validForm, $emailError;			//Use the GLOBAL Version of these variables instead of making them local
-				$emailError = "";							//Clear the error message. 
-				
-				// Remove all illegal characters from email
-				$inEmail = filter_var($inEmail, FILTER_SANITIZE_EMAIL);
+			global $validForm, $emailError;			//Use the GLOBAL Version of these variables instead of making them local
+			$emailError = "";							//Clear the error message. 
+			
+			// Remove all illegal characters from email
+			$inEmail = filter_var($inEmail, FILTER_SANITIZE_EMAIL);
 
+			if(!empty($inEmail)){
 				// Validate e-mail
 				$inEmail = filter_var($inEmail, FILTER_VALIDATE_EMAIL);
 
 				if($inEmail === false)
 				{
 					$validForm = false;
-					$emailError = "Invalid email"; 					
+					$emailError = "Invalid email. Match format: email@email.com"; 					
 				}
+			}
 		}
-			function validateHometown($hometown)
+
+		function validateHometown($hometown)
+		{
+			global $validForm, $hometownError;	
+			$hometownError = "";
+			
+			if($hometown == "")
 			{
-				global $validForm, $hometownError;	
-				$hometownError = "";
-				
-				if($hometown == "")
-				{
+				$validForm = false;
+				$hometownError = "Hometown is required";
+			}else if(!preg_match('/^[A-Za-z0-9 ,]*[A-Za-z0-9][A-Za-z0-9 ,]*$/', $hometown)){
+				$validForm = false;
+				$hometownError = "Hometown cannot contain special characters";
+			}
+		}
+
+		function validateCareerGoals($career)
+		{
+			global $validForm, $careerGoalsError;	
+			$careerGoalsError = "";
+			
+			if (strlen(trim($_POST['careerGoals']))){
+				if(!preg_match('/^[A-Za-z0-9 ,\!\?\"\_\-\'.]*$/', $career)){
 					$validForm = false;
-					$hometownError = "Name cannot be spaces";
+					$careerGoalsError = "Career goals can only contains letters, numbers, and punctuation";
 				}
 			}
-			function validateCareerGoals($career)
+		}
+
+		function validateThreeWords($words)
+		{
+			global $validForm, $threeWordsError;	
+			$threeWordsError = "";
+			
+			if(!empty($words))
 			{
-				global $validForm, $careerGoalsError;	
-				$careerGoalsError = "";
-				
-				if(strlen(trim($career)) == 0)
-				{
-					echo "inside";
+				if(!preg_match('/^[A-Za-z0-9 ,\!\?\"\_\-\'.]*$/', $words)){
 					$validForm = false;
-					$careerGoalsError = "Name cannot be spaces";
+					$threeWordsError = "Description can only contains letters, numbers, and punctuation";
 				}
 			}
-			function validateThreeWords($words)
-			{
-				global $validForm, $threeWordsError;	
-				$threeWordsError = "";
-				
-				if(empty($words))
-				{
-					$validForm = false;
-					$threeWordsError = "Name cannot be spaces";
-				}
-			}
+		}
+
+		// reCaptcha
+	    function validateRecaptcha($captcha){
+	    	global $validForm, $captchaError;
+
+	    	$secretKey = "6Le3REoUAAAAAIVoaYSGnA23Jcft4Ey5IzvojUIo";
+		    $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha);
+		    $responseKeys = json_decode($response,true);
+
+		    if($responseKeys["success"] != true) {
+		      $validForm = false;
+		      $captchaError = "Please complete the reCaptcha";
+		    }
+		    
+		    grecaptcha.reset();
+	    }
+
 		$validForm = true;		//switch for keeping track of any form validation errors
 		
 		validateFirstName($student_firstname);
@@ -191,6 +240,7 @@
 		validateHometown($student_hometown);
 		validateCareerGoals($student_careerGoals);
 		validateThreeWords($student_threeWords);
+		validateRecaptcha($captcha);
 		
 		if($validForm)
 		{
@@ -245,7 +295,14 @@ validateThreeWords
 <script>
 
 	$(document).ready(function(){
-		if( $("select[name=program]	option:selected").val() == "webDevelopment")
+
+		var programDropdown = document.getElementById("program");
+		var selectedProgram = programDropdown.options[programDropdown.selectedIndex].value;
+
+		var programDropdown2 = document.getElementById("program2");
+		var selectedProgram2 = programDropdown2.options[programDropdown2.selectedIndex].value;
+
+		if( selectedProgram == "webDevelopment" || selectedProgram2 == "webDevelopment")
 		{
 			$(".secondWeb").css("display", "inline");
 		}
@@ -255,7 +312,10 @@ validateThreeWords
 		}
 		
 		$("select#program").change(function(){
-			if( $("select#program option:checked").val() == "webDevelopment")
+			var programDropdown = document.getElementById("program");
+			var selectedProgram = programDropdown.options[programDropdown.selectedIndex].value;
+
+			if( selectedProgram == "webDevelopment")
 			{
 				$(".secondWeb").css("display", "inline");
 			}
@@ -264,8 +324,26 @@ validateThreeWords
 				$(".secondWeb").css("display", "none");
 			}
 		});
+
+		$("select#program2").change(function(){
+			var programDropdown2 = document.getElementById("program2");
+			var selectedProgram2 = programDropdown2.options[programDropdown2.selectedIndex].value;
+
+			if(selectedProgram2 == "webDevelopment")
+			{
+				$(".secondWeb").css("display", "inline");
+			}
+			else
+			{
+				$(".secondWeb").css("display", "none");
+			}
+		});
+
 		
-		function resetForm(){
+		
+	});
+
+	function resetForm(){
 			$("#firstName").val("");
 			$("#lastName").val("");
 			$("#program").val("default");
@@ -275,11 +353,13 @@ validateThreeWords
 			$("#hometown").val("");
 			$("#careerGoals").val("");
 			$("#threeWords").val("");
+			$('.error').html("");
 		}
-	});
 	
 	
 	</script>
+	<!-- reCaptcha -->
+	<script src='https://www.google.com/recaptcha/api.js'></script>
   
   <style>
 	img{
@@ -333,10 +413,10 @@ validateThreeWords
 	.violet{
 		background-image: url("ultra violet.png");
 		padding: .5em;	
-	}	
+	}
 	.secondWeb{
 		display: none;
-	}
+	}	
 	table{
 		margin: auto;
 	}
@@ -345,7 +425,6 @@ validateThreeWords
 	}
 	.error{
 		font-style: italic;
-		font-size: 18px;
 		color: #d42a58;
 		font-weight: bold;
 	}
@@ -368,10 +447,7 @@ validateThreeWords
   } 
   .violet {
 	background-image:none;  
-  }  
-  .secondWeb{
-		display: none;
-	}  
+  }   
   table{
 		margin: auto;
 	}
@@ -403,7 +479,7 @@ validateThreeWords
 
 	<div class="main form">
 	
-	<h2></h2>
+	<h2><?php echo $message; ?></h2>
 	</table>
 	<form id="portfolioBioForm" method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
 		
@@ -419,54 +495,60 @@ validateThreeWords
 		</tr>
 		<tr>
 		<td >Program:<br> <select id="program" name="program">
-				<option value="default">---Select Your Program---</option>
-				<option value="animation" >Animation</option>
-				<option value="graphicDesign" >Graphic Design</option>
-				<option value="photography" >Photography</option>
-				<option value="videoProduction" >Video Production</option>
-				<option value="webDevelopment" >Web Development</option>
+				<option value="default" >---Select Your Program---</option>
+				<option value="animation" <?php if($student_program == "animation"){echo "selected=selected";}?>>Animation</option>
+				<option value="graphicDesign" <?php if($student_program == "graphicDesign"){echo "selected=selected";}?>>Graphic Design</option>
+				<option value="photography" <?php if($student_program == "photography"){echo "selected=selected";}?>>Photography</option>
+				<option value="videoProduction" <?php if($student_program == "videoProduction"){echo "selected=selected";}?>>Video Production</option>
+				<option value="webDevelopment" <?php if($student_program == "webDevelopment"){echo "selected=selected";}?>>Web Development</option>
 			</select><br><span class="error" id="programError"><?php echo($programError);?></span><td>
 		</tr>
 		<tr>
-		<td >Secondary Program:<br> <select id="program2" name="program2"</selec>>
+		<td >Secondary Program:<br> <select id="program2" name="program2">
 				<option value="none" >---No Secondary Program---</option>
-				<option value="animation" >Animation</option>
-				<option value="graphicDesign" >Graphic Design</option>
-				<option value="photography" >Photography</option>
-				<option value="videoProduction" >Video Production</option>
-				<option value="webDevelopment" >Web Development</option>
+				<option value="animation" <?php if($student_program2 == "animation"){echo "selected=selected";}?>>Animation</option>
+				<option value="graphicDesign" <?php if($student_program2 == "graphicDesign"){echo "selected=selected";}?>>Graphic Design</option>
+				<option value="photography" <?php if($student_program2 == "photography"){echo "selected=selected";}?>>Photography</option>
+				<option value="videoProduction" <?php if($student_program2 == "videoProduction"){echo "selected=selected";}?>>Video Production</option>
+				<option value="webDevelopment" <?php if($student_program2 == "webDevelopment"){echo "selected=selected";}?>>Web Development</option>
 			</select><br><span class="error" id="program2Error"></span><td>
 		</tr>
 		<tr>
 		<td>Website Address:<br> <input type="text" id="websiteAddress" name="websiteAddress" value="<?php echo $student_websiteAddress;  ?>"/><br><span class="error" id="websiteAddressError"><?php echo($websiteAddressError);?></span></td>
 		</tr>
 		<tr>
-		<td>Personal Email:<br><input type="text" id="email" name="email" value="<?php echo $student_email;  ?>" /><br><span class="error" id="emailError"><?php echo($emailError); ?></span></td>
+		<td class="secondWeb">Secondary Website Address (git repository, etc.):<br> <input type="text" id="websiteAddress2" name="websiteAddress2" value="<?php echo $student_websiteAddress2;  ?>"/><br><span class="error" id="websiteAddress2Error"><?php echo($websiteAddress2Error);?></span></td>
 		</tr>
 		<tr>
 		<td>LinkedIn Profile:<br><input type="text" id="linkedIn" name="linkedIn" value="<?php echo $student_linkedIn;  ?>" /><br><span class="error" id="linkedInError"><?php echo($linkinError);?></span></td>
+		</tr>
 		<tr>
-		<td><span class="secondWeb">Secondary Website Address (git repository, etc.):<br> <input type="text" id="websiteAddress2" name="websiteAddress2" value="<?php echo $student_websiteAddress;  ?>"/><br><span class="error" id="websiteAddress2Error"><?php echo($websiteAddress2Error);?></span></span></td>
+		<td>Personal Email:<br><input type="text" id="email" name="email" value="<?php echo $student_email;  ?>" /><br><span class="error" id="emailError"><?php echo($emailError); ?></span></td>
 		</tr>
 		<tr>
 		<td>Hometown:<br> <input type="text" id="hometown" name="hometown" value="<?php echo $student_hometown;  ?>"/><br><span class="error" id="hometownError"><?php echo($hometownError); ?></span></td>
 		</tr>
 		<tr>
-		<td>Career Goals:<br> <textarea id="careerGoals" name="careerGoals"> </textarea><br><span class="error" id="careerGoalsError"><?php echo($careerGoalsError); ?></span></td>
+		<td>Career Goals:<br> <textarea id="careerGoals" name="careerGoals"><?php echo($student_careerGoals); ?></textarea><br><span class="error" id="careerGoalsError"><?php echo($careerGoalsError); ?></span></td>
 		</tr>
 		<tr>
 		<td>3 Words that Describe You:<br> <input type="text" id="threeWords" name="threeWords" value="<?php echo $student_threeWords;  ?>"/><br><span class="error" id="threeWordsError"><?php echo($threeWordsError); ?></span></td>
-		<p class="robotic" id="pot">
+		</tr>
+		<!-- <p class="robotic" id="pot">
 			<label>Leave Blank</label>
 			<input type="hidden" name="inRobotest" id="inRobotest" class="inRobotest" />
-		</p>
+		</p> -->
+		<tr>
+    	<td><div class="g-recaptcha" data-sitekey="6Le3REoUAAAAAEJ8etrsYzKL7ZM9ZwMRldJCQ8tz"></div>
+    	<span class="error"><?php echo $captchaError; ?></span></td>
+    	
 		<input type="hidden" id="submitConfirm" name="submitConfirm" value="submitConfirm"/>
 		</tr>
 		<tr>
 		<td><input type="submit" id="submitBio" name="submit" value="Submit Bio" /></td>
-		</tr>
+		<!-- </tr> -->
 		<tr>
-		<td><input type="reset" id="resetBio" name="resetBio" value="Reset Bio" onClick="resetForm()" /></td>
+		<td><input type="reset" id="resetBio" name="resetBio" value="Reset Bio" onclick="resetForm()"/></td>
 		</tr>
 		</table>
 	</form>
@@ -482,5 +564,6 @@ validateThreeWords
 
 </body>
 </section>
+
 
 </html>
